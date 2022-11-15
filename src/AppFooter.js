1f197ac8b0aba41d38889e86929f1822abd857d0
jun-yuaka <yuaka0511@gmail.com>
2022-11-16 06:32:28 +0900

    AppFooter追加

---
+++ src/AppFooter.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from "react";
+
+
+export default class AppFooter extends Component {
+
+  render() {
+    return (
+      <Fragment>
+        <hr />
+        <p>Copyright &copy; 2022 Acme Ltd.</p>
+      </Fragment>
+    );
+  }
+}
