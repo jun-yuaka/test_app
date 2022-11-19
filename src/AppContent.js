1c7645dfedbabd6cbed110100d46f4a1d625e7b6
jun-yuaka <yuaka0511@gmail.com>
2022-11-19 16:11:01 +0900

    props追加

--- src/AppContent.js
+++ src/AppContent.js
@@ -2,14 +2,53 @@ import React, { Component } from 'react';
 
 export default class AppContent extends Component {
 
+  state = {posts: []};
+
+  anotherFunction = () => {
+    console.log("another function")
+  }
+
+  leftParagrap = () => {
+    console.log("leftparagrap")
+  }
+
+  fetchList = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then(json => {
+          this.setState({posts: json});
+        })
+  }
+
+  clickedItem = (x) => {
+    console.log("clicked", x);
+  }
+
   render(){
     return (
-      <p>
+      <div>
         This is the content
         
         <br />
+        <hr />
+        <p onMouseEnter={this.anotherFunction} onMouseLeave={this.leftParagrap}>This is some text</p>
         
-      </p>
+        <button onClick={this.fetchList} className='btn btn-primary'>Fetch Date</button>
+
+        <hr />
+
+        <p>Post is{this.state.posts.length} items long</p>
+
+        <ul>
+          {this.state.posts.map((c) => (
+            <li key={c.id}>
+              <a href="#!" onClick={() => this.clickedItem(c.id)}>
+                {c.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
     )
   }
 }
