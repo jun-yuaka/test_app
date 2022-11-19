ed4a1e7644aabaae0af125069b7a961ceae25563
jun-yuaka <yuaka0511@gmail.com>
2022-11-19 10:41:21 +0900

    AppHeader追加

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import ReactDOM  from 'react-dom';
+import AppHeader from './AppHeader';
 import AppFooter from './AppFooter';
+import AppContent from './AppContent';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './index.css';
 
 class App extends Component {
@@ -8,9 +13,8 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <div>
-          <h1>Hello, World!</h1>
-        </div>
+        <AppHeader title="Cool App!" />
+        <AppContent />
         <AppFooter />
       </div>
     );
